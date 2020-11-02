7e445643f8d77d51e308b76e68e41bb60f69cc65
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-02 09:59:29 +0530

    [tejas] Add Insert data

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -2,16 +2,20 @@ create database AddressBook;
 
 --Create Table
 create table AddBook(
-ID int,
 First_Name varchar(20),
 Last_Name varchar(20),
 Address varchar(200),
 City varchar(20),
 State varchar(20),
-Zip int,
-Phone_Number int,
+Zip varchar(20),
+Phone_Number varchar(20),
 Email varchar(50)
 );
+select * from AddBook
+
+--Insert data
+Insert into AddBook(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email)
+Values('Sachin','Tendulkar','Bandra East','Mumbai','Maharashtra','411610','9163121771','sachin@abc.com');
 select * from AddBook
 
 
