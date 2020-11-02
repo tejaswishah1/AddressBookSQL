117a9e99122d536a4fb3ef765ffcfec9cd316a9f
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-02 09:47:38 +0530

    [tejas] add Create table

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -1,2 +1,17 @@
 create database AddressBook;
 
+--Create Table
+create table AddBook(
+ID int,
+First_Name varchar(20),
+Last_Name varchar(20),
+Address varchar(200),
+City varchar(20),
+State varchar(20),
+Zip int,
+Phone_Number int,
+Email varchar(50)
+);
+select * from AddBook
+
+
