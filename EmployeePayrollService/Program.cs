4ee0902a4fee473610057ac062ae2624c8fb662f
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-05 10:31:47 +0530

    [Tejas] add to visual studio C#

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            EmployeeRepo repo = new EmployeeRepo();
+            repo.GetAllEmployee();
+        }
+    }
+}
